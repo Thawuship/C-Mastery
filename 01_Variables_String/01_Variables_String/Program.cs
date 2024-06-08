04e155c4019617f23e548bd6548bf960af2debbb
Mohamed Thawuship <49475559+Thawuship@users.noreply.github.com>
2024-06-08 12:31:47 +0530

    init

---
+++ 01_Variables_String/01_Variables_String/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// declare a (string) variable
+string myFirstName;
+
+// assign a value to the myFirstName variable
+myFirstName = "John";
+
+// declare a variable and assign a value to it.
+string mySecondName = "Rhaw";
+Console.WriteLine(mySecondName);
+
+// use/access the variable
+Console.WriteLine(myFirstName);
+
+//overwriting the variable value
+myFirstName = "Khan";
+Console.WriteLine(myFirstName);
+
+Console.ReadKey();
