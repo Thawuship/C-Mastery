06a9913cb4d3e8fff90d93ec5a522258674fd3ce
Mohamed Thawuship <49475559+Thawuship@users.noreply.github.com>
2024-06-08 23:33:06 +0530

    double

---
+++ 04_Double/04_Double/Program.cs
@@ -0,0 +1,17 @@
+﻿// Define a variable Num1 of type Double and assign it the value 9.243
+Double Num1 = 9.243;
+
+// Define a variable Num2 of type Double and assign it the value 3.232
+Double Num2 = 3.232;
+
+// Calculate the sum of Num1 and Num2, and store the result in a variable named Total
+Double Total = Num1 + Num2;
+
+// Output the value of Total to the console
+Console.WriteLine(Total);
+
+// Round the value of Total to 2 decimal places and store the result in a variable named NewTotal
+Double NewTotal = Math.Round(Total, 2);
+
+// Output the value of NewTotal to the console
+Console.WriteLine(NewTotal);
