db0174d6234a4355d16b9881fb4e2e894f69e4c8
Mohamed Thawuship <49475559+Thawuship@users.noreply.github.com>
2024-06-08 15:18:32 +0530

    addition calculator

---
+++ 02_Simple_Calculator/02_Simple_Calculator/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Prints out whatever is in side of ()
+Console.WriteLine("Enter something!");
+
+// takes the user input and store it
+// variable with the name "userInput and the data type "string"
+// strings are used for text
+string userInput = Console.ReadLine(); 
+Console.WriteLine("You entered: " + userInput);
+Console.ReadKey();
