11ba0e96f3feaff28769708abb64d9a1d8c2a10c
Mohamed Thawuship <49475559+Thawuship@users.noreply.github.com>
2024-06-14 13:22:37 +0530

    implicit conversion

---
+++ 05_Implicit_Conversion/05_Implicit_Conversion/Program.cs
@@ -0,0 +1,16 @@
+﻿// This code demonstrates implicit type conversion in C#.
+// Implicit conversions are automatically handled by the compiler when there is no risk of data loss.
+
+// An integer variable, which can hold values from -2,147,483,648 to 2,147,483,647
+int myInt = 1111111111;
+
+// A double precision floating point variable, which can handle very large and very small numbers with fractional components
+double myDouble = 11111111111111111111.111111111111111111;
+
+// A long integer variable, which can hold larger integer values compared to int
+long myLong = 1111111111111111111;
+
+// A single precision floating point variable, which uses less memory than double but has less precision
+float myFloat = 11111.1111f;
+
+Console.ReadKey();
