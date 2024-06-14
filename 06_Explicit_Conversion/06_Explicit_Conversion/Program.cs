5ac90f7e84eb33aa8c5dcc7887cf6d85c9b7c987
Mohamed Thawuship <49475559+Thawuship@users.noreply.github.com>
2024-06-14 14:50:51 +0530

    explicit conversion

---
+++ 06_Explicit_Conversion/06_Explicit_Conversion/Program.cs
@@ -0,0 +1,23 @@
+﻿// Explicit Conversion
+
+// Declare a long variable and assign a value
+long myLong = 123233453454;
+Console.WriteLine(myLong);
+
+// Convert the long variable to an int using explicit casting
+int myInt = (int)myLong;
+Console.WriteLine(myInt);
+
+// Declare a double variable and assign a value
+double myDouble = 234.234234242;
+Console.WriteLine(myDouble);
+
+// Convert the double variable to a float using explicit casting
+float myFloat = (float)myDouble;
+Console.WriteLine(myFloat);
+
+// Convert the double variable to an int using explicit casting
+int myInts = (int)(myDouble);
+Console.WriteLine(myInts);
+
+Console.ReadKey();
